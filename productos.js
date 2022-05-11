7f317980326d98cf84a61c1b2a3458586d7228e1
isalasjemer <96083700+isalasjemer@users.noreply.github.com>
2022-05-11 00:46:07 -0300

    get by id added

--- productos.js
+++ productos.js
@@ -1,16 +1,32 @@
 const { Router } = require('express');
 const router = Router();
 
-const productos = [];
+const productos = [{ "id": 1, "title": "Charango", "price": "5000", "thumbnail": "url.com/charango" }, { "id": 2, "title": "Guitarra", "price": "25000", "thumbnail": "url.com/guitarra" }, { "id": 3, "title": "Ukelele", "price": "10000", "thumbnail": "url.com/ukelele" }];
 
-router.get('/', (req, res)=>{
-    res.json({productos: productos})
+//  devuelve todos los productos
+router.get('/', (req, res) => {
+    res.json({ productos: productos })
+});
+
+// devuelve un producto según su id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const foundProduct = productos.filter(product => product.id === parseInt(id))
+    console.log(foundProduct)
+    if(foundProduct.length > 0){
+        res.json(foundProduct)
+    }else{
+        res.json({'error': 'Producto no encontrado'})
+    }
 })
 
-router.post('/', (req, res)=>{
+
+
+router.post('/', (req, res) => {
     const producto = req.body;
+    console.log(producto)
     productos.push(producto);
-    res.send({mensaje:'producto agregado con exito'})
-})
+    res.send({ mensaje: 'producto agregado con exito' });
+});
 
 module.exports = router;
